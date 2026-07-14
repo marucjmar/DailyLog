961f460b6184ab648f5ac517e074abd4ab3f5dc2
Marcin <marcin.lazar@pm.me>
2026-07-14 21:16:30 +0200

    fix login

--- backend/lib/backend/integrations/js/templates/garmin/sync.ts
+++ backend/lib/backend/integrations/js/templates/garmin/sync.ts
@@ -1,90 +1,91 @@
-import os from 'node:os';
-import fs from 'node:fs';
+import os from "node:os";
+import fs from "node:fs";
 
-import gc, { IGarminTokens } from 'npm:garmin-connect-nexxt@1.6.19';
-import dayjs from 'npm:dayjs@1.11.21';
+import gc, { IGarminTokens } from "npm:garmin-connect-nexxt@1.6.19";
+import dayjs from "npm:dayjs@1.11.21";
 
-import type { ActivityEvent, Ctx, SyncResult } from '../../template.ts';
+import type { ActivityEvent, Ctx, SyncResult } from "../../template.ts";
 
 const { GarminConnect } = gc;
 
 // Utils
-const delay = async (delayTime: number) => await new Promise(r => setTimeout(r, delayTime));
+const delay = async (delayTime: number) =>
+  await new Promise((r) => setTimeout(r, delayTime));
 const today = dayjs();
-const isToday = (time: string) => dayjs(time).isSame(today, 'day');
+const isToday = (time: string) => dayjs(time).isSame(today, "day");
 
 // Types
 export type State = { cursor?: number; session: IGarminTokens };
 type Context = Ctx<State>;
 
 export async function sync(ctx: Context): Promise<SyncResult> {
-    const GCClient = new GarminConnect();
-    GCClient.loadToken(ctx.state.session.oauth1, ctx.state.session.oauth2);
+  const start: number = ctx.state?.cursor ?? 0;
+  const activitiesCount = ctx.trigger === "sync_all" ? 5 : 3;
 
-    let activitiesCount = 3;
-    const start: number = ctx.state?.cursor ?? 0;
+  const GCClient = new GarminConnect();
+  GCClient.loadToken(ctx.state.session.oauth1, ctx.state.session.oauth2);
 
-    if (ctx.trigger === 'sync_all') {
-      activitiesCount = 5;
+  const activities = await GCClient.getActivities(start, activitiesCount);
+  const events: ActivityEvent[] = [];
+
+  let dailySyncContinuation = activities.length > 0;
+
+  for (const activity of activities) {
+    //@ts-ignore
+    const endTimeGMT = activity.endTimeGMT;
+
+    if (
+      ctx.trigger === "daily_sync" &&
+      !isToday(activity.startTimeGMT) &&
+      !isToday(endTimeGMT)
+    ) {
+      dailySyncContinuation = false;
+      continue;
     }
 
-    await GCClient.login();
-    await delay(1000);
+    await GCClient.downloadOriginalActivityData(activity, os.tmpdir(), "gpx");
+    const gpx = fs.readFileSync(`${os.tmpdir()}/${activity.activityId}.gpx`);
 
-    const activities = await GCClient.getActivities(start, activitiesCount);
-    const events: ActivityEvent[] = [];
+    const event: ActivityEvent = {
+      type: "activity",
+      unique_hash: `activity:garmin:${activity.activityId}`,
+      time: {
+        start: new Date(activity.startTimeGMT).getTime(),
+        end: new Date(endTimeGMT).getTime(),
+      },
+      source: "garmin",
+      preview_url: `https://connect.garmin.com/app/activity/${activity.activityId}`,
+      payload: {
+        activity_id: activity.activityId.toString(),
+        sport: activity.activityType.typeKey,
+        distance: activity.distance,
+        duration: activity.duration,
+        gpx: gpx.toString(),
+      },
+    };
 
-    let dailySyncContinuation = activities.length > 0;
+    events.push(event);
 
-    for (const activity of activities) {
-      //@ts-ignore
-      const endTimeGMT = activity.endTimeGMT;
-      
-      if (ctx.trigger === 'daily_sync' && !isToday(activity.startTimeGMT) && !isToday(endTimeGMT)) {
-        dailySyncContinuation = false;
-        continue;
-      }
+    await delay(2000);
+  }
 
-      await GCClient.downloadOriginalActivityData(activity, os.tmpdir(), 'gpx');
-      const gpx = fs.readFileSync(`${os.tmpdir()}/${activity.activityId}.gpx`);
+  let continuation: SyncResult<State>["continuation"] = undefined;
 
-      const event: ActivityEvent = {
-        type: 'activity',
-        unique_hash: `activity:garmin:${activity.activityId}`,
-        time: {
-          start: new Date(activity.startTimeGMT).getTime(),
-          end: new Date(endTimeGMT).getTime()
-        },
-        source: 'garmin',
-        preview_url: `https://connect.garmin.com/app/activity/${activity.activityId}`,
-        payload: {
-          activity_id: activity.activityId.toString(),
-          sport: activity.activityType.typeKey,
-          distance: activity.distance,
-          duration: activity.duration,
-          gpx: gpx.toString(),
-        }
-      };
+  if (
+    (ctx.trigger === "sync_all" && dailySyncContinuation) ||
+    (ctx.trigger === "sync_all" && activities.length === activitiesCount)
+  ) {
+    continuation = {
+      state: {
+        ...ctx.state,
+        session: GCClient.exportToken(),
+        cursor: start + activitiesCount,
+      },
+      schedule: {
+        delayMs: 2 * 60 * 1000,
+      },
+    };
+  }
 
-      events.push(event);
-
-      await delay(2000);
-    }
-
-    let continuation: SyncResult<State>['continuation'] = undefined;
-
-    if ((ctx.trigger === 'sync_all' && dailySyncContinuation) || (ctx.trigger === 'sync_all' && activities.length === activitiesCount)) {
-      continuation = {
-        state: {
-          ...ctx.state,
-          session: GCClient.exportToken(),
-          cursor: start + activitiesCount,
-        },
-        schedule: {
-          delayMs: 2 * 60 * 1000
-        }
-      }
-    }
-
-    return { events: await Promise.all(events), continuation };
-};
+  return { events: await Promise.all(events), continuation };
+}
